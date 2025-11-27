8a69c6e6bc19815b9a95b6132654ed2ab2ddfd51
AlbieTappenden <albie2.tappenden@live.uwe.ac.uk>
2025-11-27 17:55:28 +0000

    Update sketch.js

--- Week04 - Behaviours/sketch.js
+++ Week04 - Behaviours/sketch.js
@@ -1,17 +1,57 @@
+
+// TASK 1 - 2
+
+// function setup()
+// {
+//   createCanvas(500, 500);
+//   background(0);
+//   fill(0, 0, 255);
+// }
+
+// function draw()
+// {
+//   for (let y = 25; y < height; y += 50)
+//   {
+//     for (let x = 25; x < width; x += 50)
+//     {
+//       fill(random(10), random(255), random(255));
+
+//       ellipse(x, y, 50, 50);
+//     }
+//   }
+// }
+
+// TASK 3
+
 function setup()
 {
   createCanvas(500, 500);
-  background(0);
-  fill(0, 0, 255);
 }
 
 function draw()
 {
-  for (let y = 25; y < height; y += 50)
+  background(0);
+  let unit = 50;
+
+  for (let y = unit; y < height; y += unit)
   {
-    for (let x = 25; x < width; x += 50)
+    for (let x = unit; x < width; x += unit)
     {
-      ellipse(x, y, 50, 50);
+      herringBone(x, y, unit);
     }
   }
 }
+
+function herringBone(xVal, yVal, unit)
+{
+  stroke("255");
+  strokeWeight(2);
+
+  line(xVal, yVal, xVal - unit, yVal + unit);
+  line(xVal, yVal, xVal + unit, yVal + unit);
+  line(xVal, yVal - unit, xVal - unit, yVal);
+  line(xVal, yVal - unit, xVal + unit, yVal);
+}
+
+
+// TASK 4
