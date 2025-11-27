c163f2adff96ac1defd0e5273f76b9bb3da1d731
AlbieTappenden <albie2.tappenden@live.uwe.ac.uk>
2025-11-27 18:29:08 +0000

    glitchimages uploaded

--- week08 - GlitchImages/sketch.js
+++ week08 - GlitchImages/sketch.js
@@ -1,43 +1,26 @@
-let dotsArray = [] 
+let glitch;
+
 function setup()
 {
-  let w = width;
-  let h = height;
-  createCanvas(710, 400);
-  noFill()
-  stroke(255,0,);
-  for (let i = 0; i < 50; i++)
-  {
-    let ellipseObj =
-    {
-      x: random(w),
-      y: random(h),
-      diameter: random(10, 50),
-      speedX: random(-2, 2),
-      speedY: random(-2, 2),
-    };
-    dotsArray.push(ellipseObj);
-  }
-  console.log(dotsArray);
-}
-function draw()
-{
-  background(0);
-  for (let i = 0; i < 50; i++)
-  {
-    ellipseObj = dotsArray[i];
-    ellipseObj.x += ellipseObj.speedX;
-    ellipseObj.y += ellipseObj.speedY;
-    if (ellipseObj.x < 0 || ellipseObj.x > width)
-    {
-      ellipseObj.speedX *= -1;
-    }
-    if (ellipseObj.y < 0 || ellipseObj.y > height)
-    {
-      ellipseObj.speedY *= -1;
-    }
-    stroke("blue");
-    ellipse(ellipseObj.x, ellipseObj.y, ellipseObj.diameter);
-  }
+	createCanvas(windowWidth, windowHeight);
+	background(0);
+	imageMode(CENTER);
+
+    
+	glitch = new Glitch();
+	loadImage('data/images/fishv2.png', function(im)
+	{
+		glitch.loadImage(im);
+	});
+    glitch.debug();
 }
 
+function draw()
+{
+	glitch.resetBytes();
+	glitch.randomBytes(10);
+    glitch.replaceBytes(1, 1);
+
+	glitch.buildImage();
+	image(glitch.image, width/2, height/2)
+}
