b91ff192e61c4a6a27be64d87a9b0a4444675535
AlbieTappenden <albie2.tappenden@live.uwe.ac.uk>
2025-12-01 22:03:15 +0000

    Update sketch.js

--- GenerativeDrawingProject/Repetition/sketch.js
+++ GenerativeDrawingProject/Repetition/sketch.js
@@ -1,26 +1,34 @@
-
-function setup()
+function setup() 
 {
-	createCanvas(700, 700);
-	angleMode(DEGREES);
-	rectMode(CENTER);
-	background("black");
-    console.log("Repetition Canvas created successfully");
+  createCanvas(800, 800);
+//  noStroke(); - commented out because it gave it a cool outline to each object somehow.
+
+  let grid = 10; // a 10 x 10 grid
+  let size = width / grid; // how big each cell is
+
+  for (let y = 0; y < grid; y++) 
+  {
+    for (let x = 0; x < grid; x++) 
+    {
+
+      // Random colours for square and circle
+      fill(random(255), random(255), random(255));
+      rect(x * size, y * size, size, size);
+
+      fill(random(255), random(255), random(255));
+      ellipse(x * size + size / 2, y * size + size / 2, size * 0.7);
+    }
+  }
 }
 
 function draw()
 {
-  translate(width/2, height/2);
-  for (let squareSize = 0; squareSize < 20; squareSize = squareSize + 1 )
-  {
-    for (let angle = 0; angle < 100; angle += 0.5)
-    {
-      stroke("cyan");
-      strokeWeight(0.4);
-      noFill(0);
-      rotate(angle);
-      scale(0.99)
-      ellipse(0,0, width, height); 
-    }
-  }
+
 }
+
+// for this piece, I have taken insperation from the hungarian-french
+// artist Victor Vasarely who specialised in creating art that tricked
+// the eyes most commonly, but for this specific piece i recreated a
+// simplified version of his style by using coloured squares and circles
+// inside them. And how when the page is refreshed, each time the colours
+// will be new.
